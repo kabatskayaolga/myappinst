c3525c4f137c22d346fc824f728a0fae84830a8a
kabatskayaolga <kabatskaya.olga@gmail.com>
2021-06-28 19:51:39 +0300

    create project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React, {  useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetch("https://api.instagram.com/oauth/authorize?client_id=101657562183940&redirect_uri=https://kabatskayaolga.github.io/myappinst/&scope=user_profile,user_media&response_type=code")
+      .then(res => res.json())
+      .then(
+        (result) => {
+         console.log(result)
+        },
+        // Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
+        // чтобы не перехватывать исключения из ошибок в самих компонентах.
+        (error) => {
+          console.log(error)
+        }
+      )
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
